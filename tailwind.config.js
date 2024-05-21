8238739fc96cdb4660d70ec597b922461df05cb3
Facug03 <facundogonzales1550@gmail.com>
2024-05-21 14:36:46 -0300

    add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blink: {
+          '0%, 50%': { visibility: 'hidden' },
+          '50.01%, 100%': { visibility: 'visible' },
+        },
+      },
+      animation: {
+        blink: 'blink 1.4s step-start infinite',
+      },
+    },
   },
   plugins: [],
 }
