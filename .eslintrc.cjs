775979debf36966378e0be2724a2443c49d25f4c
Facug03 <facundogonzales1550@gmail.com>
2024-05-22 18:56:36 -0300

    add prettier, standard and some rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,15 +5,18 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'standard',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'eslint-plugin-react-compiler'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react-compiler/react-compiler': 'error',
+    'no-unused-vars': 'error',
+    'semi': ['warn', 'never'],
+    'no-eval': 'off',
+    'no-undef': 'off',
   },
 }
