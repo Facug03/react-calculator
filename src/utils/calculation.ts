34d4d544d0237165ca2829ab351a5f3016cdd8f1
Facug03 <facundogonzales1550@gmail.com>
2024-05-22 18:18:09 -0300

    add new function

--- src/utils/calculation.ts
+++ src/utils/calculation.ts
@@ -1,3 +1,7 @@
 export const splitNumbers = (value: string) => {
   return value.split(/([^\d,.]+)/)
 }
+
+export const stringToNumberFormat = (value: string) => {
+  return value.replace(/,/g, '')
+}
