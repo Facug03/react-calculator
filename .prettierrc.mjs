49dc782115c9489f7403afbb6ccb3c1ff70070f4
Facug03 <facundogonzales1550@gmail.com>
2024-05-22 18:56:52 -0300

    add prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  quoteProps: "consistent",
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+  endOfLine: "lf",
+  arrowParens: "always",
+};
