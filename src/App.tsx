22ff67337993ba8f54a4127b7c4d360079d717d9
Facug03 <facundogonzales1550@gmail.com>
2024-05-21 22:32:26 -0300

    add on click to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,105 @@
 import { useCalculate } from './hooks/useCalculate'
+import { useToast } from './hooks/useToast'
 import { Button } from './ui/Button'
 
 function App() {
-  const { formAction } = useCalculate()
+  const { value, onClick, error } = useCalculate()
+  const ref = useToast(error)
 
   return (
     <main className='grid place-content-center w-full h-screen'>
-      <form
-        action={formAction}
-        className='flex flex-col justify-center gap-10 border border-slate-950 h-[550px] py-2 px-5 bg-slate-950 rounded-xl max-w-80'
-      >
-        <div className='flex items-center h-14'>
-          <input className='block bg-transparent outline-none cursor-default pointer-events-none text-white text-3xl w-full' />
+      <form className='flex flex-col justify-center gap-10 border border-slate-950 h-[550px] py-2 px-5 bg-slate-950 rounded-xl max-w-80 relative'>
+        <div className='flex items-center h-20'>
+          <input
+            name='calculation'
+            className='block bg-transparent outline-none cursor-default pointer-events-none text-white text-3xl w-full text-right'
+            value={value}
+            readOnly
+          />
           <span className='text-white text-3xl animate-blink'>|</span>
         </div>
 
         <hr />
 
         <section className='grid grid-cols-4 gap-1 items-baseline'>
-          <Button bgColor='secondary' color='danger'>
+          <Button onClick={onClick} bgColor='secondary' color='danger'>
             C
           </Button>
-          <Button bgColor='secondary' color='secondary'>
+          <Button onClick={onClick} bgColor='secondary' color='secondary'>
             {'<-'}
           </Button>
-          <Button bgColor='secondary' color='secondary'>
+          <Button onClick={onClick} bgColor='secondary' color='secondary'>
             /
           </Button>
-          <Button bgColor='secondary' color='secondary'>
+          <Button onClick={onClick} bgColor='secondary' color='secondary'>
             *
           </Button>
 
-          <Button bgColor='primary' color='primary'>
+          <Button onClick={onClick} bgColor='primary' color='primary'>
             7
           </Button>
-          <Button bgColor='primary' color='primary'>
+          <Button onClick={onClick} bgColor='primary' color='primary'>
             8
           </Button>
-          <Button bgColor='primary' color='primary'>
+          <Button onClick={onClick} bgColor='primary' color='primary'>
             9
           </Button>
-          <Button bgColor='secondary' color='secondary'>
+          <Button onClick={onClick} bgColor='secondary' color='secondary'>
             -
           </Button>
 
-          <Button bgColor='primary' color='primary'>
+          <Button onClick={onClick} bgColor='primary' color='primary'>
             4
           </Button>
-          <Button bgColor='primary' color='primary'>
+          <Button onClick={onClick} bgColor='primary' color='primary'>
             5
           </Button>
-          <Button bgColor='primary' color='primary'>
+          <Button onClick={onClick} bgColor='primary' color='primary'>
             6
           </Button>
-          <Button bgColor='secondary' color='secondary'>
+          <Button onClick={onClick} bgColor='secondary' color='secondary'>
             +
           </Button>
 
-          <Button bgColor='primary' color='primary'>
+          <Button onClick={onClick} bgColor='primary' color='primary'>
             1
           </Button>
-          <Button bgColor='primary' color='primary'>
+          <Button onClick={onClick} bgColor='primary' color='primary'>
             2
           </Button>
-          <Button bgColor='primary' color='primary'>
+          <Button onClick={onClick} bgColor='primary' color='primary'>
             3
           </Button>
 
-          <Button gridVariant='variant-1' bgColor='primary' color='primary'>
+          <Button
+            onClick={onClick}
+            gridVariant='variant-1'
+            bgColor='primary'
+            color='primary'
+          >
             0
           </Button>
-          <Button bgColor='primary' color='primary'>
+          <Button onClick={onClick} bgColor='primary' color='primary'>
             .
           </Button>
-          <Button gridVariant='variant-2' bgColor='secondary' color='secondary'>
+          <Button
+            onClick={onClick}
+            gridVariant='variant-2'
+            bgColor='secondary'
+            color='secondary'
+          >
             =
           </Button>
         </section>
+
+        <div
+          className='max-w-xs bg-gray-500 text-sm text-white rounded-3xl shadow-lg absolute bottom-10 translate-x-1/2 transition duration-300 ease-in-out pointer-events-none'
+          style={{ opacity: 0 }}
+          role='alert'
+          ref={ref}
+        >
+          <div className='flex py-2 px-4'>Formato inválido.</div>
+        </div>
       </form>
     </main>
   )
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -2,17 +2,28 @@ import { colors, bgColors, grid } from '../consts/styles'
 
 interface Props {
   children: React.ReactNode
+  onClick: (value: string) => void
   color?: keyof typeof colors
   bgColor?: keyof typeof bgColors
   gridVariant?: keyof typeof grid
 }
 
-export function Button({ children, bgColor, color, gridVariant }: Props) {
+export function Button({
+  children,
+  onClick,
+  bgColor,
+  color,
+  gridVariant,
+}: Props) {
   return (
     <button
       className={`inline-flex items-center justify-center w-full h-16 rounded-full font-semibold text-lg ${
         bgColor && bgColors[bgColor]
       } ${color && colors[color]} ${gridVariant && grid[gridVariant]}`}
+      onClick={(e) => {
+        e.preventDefault()
+        onClick(children as string)
+      }}
     >
       {children}
     </button>
