82bb908a3eb4f8f735915586d76351e19773429d
Facug03 <facundogonzales1550@gmail.com>
2024-05-22 16:08:14 -0300

    remove compiler config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const ReactCompilerConfig = {
-  compilationMode: 'all',
-}
+const ReactCompilerConfig = {}
 
 // https://vitejs.dev/config/
 export default defineConfig({
