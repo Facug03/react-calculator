b5d5effb5c2d954f728d573c1ff52d0f3002d22c
Facug03 <facundogonzales1550@gmail.com>
2024-08-06 15:47:03 -0300

    run deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      animation: {
+        blink: 'blink 1.4s step-start infinite',
+      },
       keyframes: {
         blink: {
           '0%, 50%': { visibility: 'hidden' },
           '50.01%, 100%': { visibility: 'visible' },
         },
-      },
-      animation: {
-        blink: 'blink 1.4s step-start infinite',
       },
     },
   },
